1157ed31a0d6332f5088ea6b7cccdad042107d06
Sanskriti Bhatnagar <61701927+b-sanskriti26@users.noreply.github.com>
2022-10-14 23:44:05 +0530

    Time: 43 ms (9.08%), Space: 54 MB (18.44%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+       // int k=0;
+        HashSet<Integer> set=new HashSet<Integer>();
+        for(int i:nums){
+            if(set.contains(i)){
+                set.remove(i);
+            }else{
+                set.add(i);
+            }
+        }
+        for(int k:set){
+            return k;
+        }
+        return -1;
+    }
+}
